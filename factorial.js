a1e2c1e4c68182431bb47ffc059294fbb251be77
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-11-16 17:39:42 +0400

    Factorial

---
+++ factorial.js
@@ -0,0 +1,17 @@
+
+
+function factorial(num){
+    let result = num;
+    if (num === 0 || num === 1)
+    return 1;
+
+    while (num > 1) {
+        num--;
+        result=result*num;  
+    }
+    return result;
+}
+
+module.exports = {
+    factorial: factorial 
+};
