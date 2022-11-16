4a69299bb8ae20d3628eb8cf63565c9ebc6e72e5
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-11-16 18:43:56 +0400

    Added Comments

--- tester.js
+++ tester.js
@@ -1,8 +1,8 @@
-let num = 10; 
-const num1 = 789;
-const num2 = 46;
+let num = 3; 
+const num1 = 92;
+const num2 = 70;
 const gcd = [num1, num2];
-let number = 324;
+let number = 14;
 
 
 const factorial = require("./factorial").factorial
@@ -12,8 +12,8 @@ const decToHex = require("./hex").decToHex
 
 
 
-console.log(factorial(num));
-console.log(GCD(gcd));
-console.log(LCM(num1, num2));
+console.log(`The factorial of ${num} is ${factorial(num)}`);
+console.log(`The greatest common divisor of ${gcd} is ${GCD(gcd)}`);
+console.log(`The least common multiple is ${LCM(num1, num2)}`);
 console.log(decToHex(number))
 
